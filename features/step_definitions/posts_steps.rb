86feeddc1522f905f0826381798ff1b314b02db7
Kyle Rogers <kyle.rogers@gmail.com>
2011-04-24 21:57:29 -0500

    updated features.  Probably need to write a lot more now.

--- features/step_definitions/posts_steps.rb
+++ features/step_definitions/posts_steps.rb
@@ -1,3 +1,17 @@
 When /^I display the post$/ do
   visit "/posts/#{@post.id}"
 end
+
+Given /^I have a post$/ do
+  @post = Post.new
+end
+
+Given /^my post has a title "([^"]*)"$/ do |title|
+  @post.title = title
+  @post.save
+end
+
+Given /^my post has body "([^"]*)"$/ do |body|
+  @post.body = body
+  @post.save
+end
