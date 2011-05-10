56ece6d8a7573f821ecc0626b48554862188e380
Kyle Rogers <kyle.rogers@gmail.com>
2011-05-09 23:14:42 -0500

    playing with css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout
+
+    private
+
+    def layout
+      # or turn layout off for every devise controller:
+      devise_controller? && "admin"
+    end
 end
